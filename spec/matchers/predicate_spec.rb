000dfb4f747429ef92a1e2cc4362309c19a0f738
Luiz Felipe <23662020+lffg@users.noreply.github.com>
2020-01-04 12:42:58 -0300

    Add predicate matchers spec

---
+++ spec/matchers/predicate_spec.rb
@@ -0,0 +1,11 @@
+describe "Predicate Matchers" do
+  # Some Ruby objects have some predicate methods. `Integer`, for example, has a
+  # lot of them, such as `.even?`, `.odd?`, and more. This kind of method, which
+  # has its name ending with `?` always returns a boolean, and are called
+  # "predicate methods". RSpec convert those predicate methods to matchers, by
+  # removing the `?` from its name and prefixing it with `be_`. For example:
+  it "should BE_EVEN" do
+    expect(2.even?).to be true
+    expect(2).to be_even
+  end
+end
