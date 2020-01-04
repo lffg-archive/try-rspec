9c2bdcc84fdc2bf434b16ca9b2edeae6ba2ee70f
Luiz Felipe <23662020+lffg@users.noreply.github.com>
2020-01-04 01:38:16 -0300

    Add equality matchers

---
+++ spec/matchers/equality_spec.rb
@@ -0,0 +1,14 @@
+describe 'Equality Matchers' do
+  a = "1"
+  b = "1"
+
+  it "two strings should not be EQUAL / BE" do
+    expect(a).not_to equal(b)
+    expect(a).not_to be(b)
+  end
+
+  it "two strings should be EQL / EQ" do
+    expect(a).to eql(b)
+    expect(a).to eq(b)
+  end
+end
