17f8af5330b044a677e51e846079c79b3ee3aa8f
Luiz Felipe <23662020+lffg@users.noreply.github.com>
2020-01-05 23:17:20 -0300

    Add negated matcher example

---
+++ spec/matchers/negated_matchers_spec.rb
@@ -0,0 +1,11 @@
+# Here we create a negated matcher called `not_include`, which is the oposite of
+# the `include` matcher. It is possible to assign any name to the negated
+# matcher. In this example, `not_include` was used, as it's the best name for
+# the situation.
+RSpec::Matchers.define_negated_matcher :not_include, :include
+
+describe "Negated Matchers" do
+  it "should not_include something" do
+    expect([1, 2, 3]).to not_include(4)
+  end
+end
