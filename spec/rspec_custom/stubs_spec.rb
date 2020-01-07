96a66bc7b168814c011cdcb4020d044398383d2a
Luiz Felipe <23662020+lffg@users.noreply.github.com>
2020-01-07 00:35:53 -0300

    Add example with allow_any_instance_of

--- spec/rspec_custom/stubs_spec.rb
+++ spec/rspec_custom/stubs_spec.rb
@@ -1,21 +1,31 @@
-class Test; end
+class Test
+  def foo(some_arg); end
+end
 
 describe "Stubs" do
   let(:instance) { Test.new }
 
   example "normal stub" do
-    allow(instance).to receive(:method) # Default one.
-    allow(instance).to receive(:method).with(5).and_return("Received 5")
+    allow(instance).to receive(:foo) # Default one.
+    allow(instance).to receive(:foo).with(5).and_return("Received 5")
 
-    expect(instance.method(5)).to eq("Received 5")
-    expect(instance.method(6)).not_to eq("Received 5")
+    expect(instance.foo(5)).to eq("Received 5")
+    expect(instance.foo(6)).not_to eq("Received 5")
   end
 
   example "block stubs" do
-    allow(instance).to receive(:method) do |given_arg|
+    allow(instance).to receive(:foo) do |given_arg|
       "Received #{given_arg}"
     end
 
-    expect(instance.method(5)).to eq("Received 5")
+    expect(instance.foo(5)).to eq("Received 5")
+  end
+
+  example "allow_any_instance_of stub" do
+    allow_any_instance_of(Test).to receive(:foo) { |_instance, arg| "Received #{arg}" }
+
+    [Test.new, Test.new].each_with_index do |instance, index|
+      expect(Test.new().foo(index)).to eq("Received #{index}")
+    end
   end
 end
